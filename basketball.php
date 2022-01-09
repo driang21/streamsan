f0be66a40b33393fa41d7b531a6f75f050ac26fd
driang21 <97405148+driang21@users.noreply.github.com>
2022-01-10 02:13:40 +0700

    Add files via upload

---
+++ basketball.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html class="no-js en">
+    
+<!-- Mirrored from ug.vloxyze.xyz/basketball.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 09 Jan 2022 01:21:25 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1,user-scalable=1">
+    <meta name="robots" content="NOINDEX, NOFOLLOW, NOARCHIVE, NOODP, NOIMAGEINDEX, NOSNIPPET">
+    <title>Hs Basketball | Streamsan.com</title>
+    <link rel=stylesheet href="https://ug.vloxyze.xyz/css/main.css?cache_control=1">
+    <script src="https://ug.vloxyze.xyz/js/modernizr.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel=stylesheet >
+    <link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" type="image/png" href="https://ug.vloxyze.xyz/img/x.png">
+    <body>
+        <style>
+            body {
+                background: linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url(https://ug.vloxyze.xyz/index/img/hs-sports-season.jpg) no-repeat center center fixed;
+                background-size: cover;
+            }
+            header {
+                background: url(https://ug.vloxyze.xyz/img/bsketball-header.jpg);
+            }
+            .popup-time {
+                background: url(https://ug.vloxyze.xyz/img/basketball1.jpg) no-repeat center center;
+                background-size: cover;
+            }
+            .popup-container {
+                background: linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url(https://ug.vloxyze.xyz/img/basketball2.jpg) no-repeat center center;
+                background-size: cover;
+       </style>
+    <div class="body-div">
+    <header id="header" class="color-span"><div class="row"><div class="large-12 columns"><div class="row"><div class="logo logo-triangle"><img src="https://ug.vloxyze.xyz/index/img/giphy.gif" alt=""><p><span class="logo-text">HS BASKETBALL</span></p></p></div></div></div> </div><div class="color-span-wrap color-span-wrap--bottom"><div></div><div></div><div></div><div></div><div></div></div></header>
+    <div align=center style="font-size:28px;color:#ffffff;font-weight:bold;margin-top:25px;margin-right:10px;margin-left:10px;">
+        </div>
+    <div class="container"> <div class="video row"> <div class="video-screen">
+        <div class="overlay-video"></div>
+        <div class="percent-container"><p><img class="loader" src="https://ug.vloxyze.xyz/img/video-throbber.gif" alt=""></p><p class="percent"></p></div>
+        <div class="popup-time"><p>Starts in <span id="countdown">00:11</span></p></div>
+        <div class="popup-container"><div class="popup-text">High School Basketball Live Is Only Available For Registered Users. Create A FREE Account To Get Access Your HD Streams</div>
+    <a href="https://ug.vloxyze.xyz/register.php" class="x-domain"><div class="popup-button">Create my account</div></a></div></div><div class="controls"><div class="small-1 medium-1 large-1 no-col-hd-pad columns pause-play"><i class="icon-play"></i><a href="https://ug.vloxyze.xyz/register.php" class="x-domain"><i class="icon-pause"></i></a></div>
+    <div class="small-8 medium-7 large-6 columns"><div class="time"><div class="time-full buffer-play"></div><div class="time-buffer buffer"></div></div></div><div class="small-1 medium-1 columns hide-for-large-up volume-icon"><i class="icon-vol"></i></div><div class="small-0 medium-0 large-3 columns no-col-left-pad show-for-large-up vol-wrapper"><div class="small-0 medium-0 large-1 columns"><i class="icon-vol"></i></div><div class="small-0 medium-0 medium-9 columns"><input type="text" data-slider="true" value="0.4" data-slider-highlight="true" style="display:none;"></div></div>
+    <a href="https://ug.vloxyze.xyz/register.php" class="x-domain fullscreen"><i class="icon-fullscreen"></i></a>
+    <div class="small-1 medium-1 large-1 no-col-hd-pad columns live-msg"><img src="https://ug.vloxyze.xyz/img/red-dot.png" class="red-dot"> LIVE</div></div></div>
+    <script src="https://ug.vloxyze.xyz/js/main.js?cache_control=&amp;lng=EN"></script>
+    <script src=http://ajax.googleapis.com/ajax/libs/webfont/1.4.2/webfont.js ></script>
+    <script>WebFont.load({google: {families: ['Open Sans']}});$(document).foundation();</script>
+    </body>
+    <center>
+    <div id="google_translate_element"></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+
+
+<center>
+
+</div>
+
+    </p>
+    
+    
+    <p>
+
+            <center>
+                <a href="https://ug.vloxyze.xyz/register.php">
+                    <button class="btn btn-primary" type="button">
+                        Watch HD 
+                    </button>
+                    <h4 style="font-size: 10px;"">
+      
+                </a>
+                <center>
+      			</div>   
+      			    </p>
+
+    <p>
+
+                <center>
+<script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    </p>
+    <a href="https://ug.vloxyze.xyz/register.php"><img src="https://ug.vloxyze.xyz/img/hd.png"></a></p>
+<footer align="center" class="col-md-12" style="background:black;padding: 20px;color: white;font-weight: bolder;text-shadow: 0px  0px 1px rgb(0, 0, 0);">
+<a data-toggle="modal" data-target="#myDMCA" href="https://ug.vloxyze.xyz/register.php">Home</a> • <a data-toggle="modal" data-target="#myDMCA" href="https://ug.vloxyze.xyz/register.php">DMCA</a> • <a data-toggle="modal" data-target="#myContact" href="https://ug.vloxyze.xyz/register.php">Contact</a>
+<div class="col-sm-6"><p>Copyright &copy; 2022 - All Rights Reserved</p></div></footer></div></div>
+<style>
+	.online{color:#fff; font-size: 12px;position:fixed; bottom: 0;right:0; background:rgba(0,0,0,0.5);  width:190px; padding:10px; border-radius:5px 0 0 0;  }
+	.online-icon{display: inline-block; background: url('https://ug.vloxyze.xyz/') no-repeat; width:14px; height:20px;}
+	.online-text{
+					width:145px;float:right;display: inline-block;
+	}
+	.online-text p{margin:0;}
+</style>
+<div class="online">
+				<span class="online-icon"></span>
+				<div class="online-text">
+				<div id="counter" data-min="12234" data-max="12733">
+				<span class="globe">
+					<span class="fa fa-globe"></span>
+				</span>
+				<span class="counter-value"><script type="text/javascript"> document.write(Math.floor(Math.random()*12733)); </script></span> Members Online Now
+			</div></div>
+
+
+</div>
